1005ba039c9d5b6db84a3099c6d10841e612fc94
JBUinfo <37612885+JBUinfo@users.noreply.github.com>
2019-10-24 17:31:01 +0200

    Update makeSync.js

--- makeSync.js
+++ makeSync.js
@@ -1,45 +1,31 @@
+const request = require('request');
+
 function sleep(ms){
   return new Promise(resolve=>{
     setTimeout(resolve,ms)
   })
 }
 
-const test = class {
-  constructor() {
-  }
-  async then(resolve, reject) {
-    try {
-      let status = true;
-     // for(let i = 0; i < 20; i++) {
-        request({options},
-        function(err, res) {
-          if(!err) {
-            if(res.statusCode == 200) {
-            }
-          }
-          if(??) status = false;
-        })
-        .catch(function (err) {
-          if(??) status = false;
-        });
-    //  }
-
-      while(status) {
-        await sleep(1000);
-      };
-      resolve();
+async function test(){
+  let flag = true;
+  try{
+    request({options},function(err, res, body) {
+      //Your code
+      flag = false;
+    })
+    while(flag){
+      await sleep(1000);
     }
-    catch(e) {
-      log(e);
-    }
+  } catch (e){
+    console.log(e);
   }
-};
+}
 
 (async() => {
   try {
-    let equisde = await new test();
+    await test();
   }
   catch(e) {
-    log(e);
+    console.log(e);
   }
 })();
